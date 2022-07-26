4db2a76dc30707120838c9d958839d0d44b563b7
Strategy102 <strategy954@gmail.com>
2022-07-26 05:27:09 -0700

    school website

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    }                       
+    const showMenu = (toggleId, navId) => {
+        const toggle = document.getElementById(toggleId);
+       const  nav = document.getElementById(navId);
+        if (toggle && nav) {
+            toggle.addEventListener('click', ()=> {
+                nav.classList.toggle('show'); //js css to display menu
+            });
+        }
+    };
+    
+    showMenu('nav-toggle', 'nav-menu');
+    
+    
+    const navLink = document.querySelectorAll('.nav__link');
+    
+    console.log({ navLink });
+    
+    function linkAction() {
+        const navMenu = document.getElementById('nav-menu');
+        navMenu.classList.remove('show'); //When we click on each nav__link, we remove the show class
+    }
+    navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+    var swiper = new Swiper(".home-slider", {
+        autoHeight: true,
+        loop:true,
+        spaceBetween: 20,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    
